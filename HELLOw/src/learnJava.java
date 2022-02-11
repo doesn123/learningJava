7dc7545b2d623f4008f4c3e97140ec7ad3625516
George <georgehadow@gmail.com>
2022-02-11 17:02:51 +0100

    first commit

---
+++ HELLOw/src/learnJava.java
@@ -0,0 +1,14 @@
+
+public class learnJava {
+
+	public static void main(String[] args) {
+		String name = "George";
+		System.out.println(name.toUpperCase());
+		
+	}
+	
+	public static void addExP(String s) {
+		System.out.println(s + "!");
+	}
+
+}
